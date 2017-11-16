6688b7840b6c01fe0fdca5457d398e05e4f4c3eb
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-16 20:04:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,9 @@ require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0
+    puts "Fizz"
   end
+  if int % 5 == 0
+    puts "Buzz"
 end
