4de043f5553cbec2e373eb46f92e200831fcd33e
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-16 20:07:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(num)
+  if num % 5 == 0 && num % 3 == 0
+    puts "FizzBuzz"
+  elseif num % 3 == 0
     puts "Fizz"
+  elseif num % 5 == 0
+    puts "Buzz"
+  else
+    return nil
   end
 end
