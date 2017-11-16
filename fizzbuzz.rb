3215c507277f8be1ecdcdd6c7e6c508b1dd57f48
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-16 20:07:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if num % 5 == 0 && num % 3 == 0
+  if num% 5 == 0 && num% 3 == 0
     "FizzBuzz"
-  elseif num % 3 == 0
+  elseif num% 3 == 0
     "Fizz"
-  elseif num % 5 == 0
+  elseif num% 5 == 0
     "Buzz"
   else
     return nil
